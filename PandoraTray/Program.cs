1c70606247d79bc126c3a16410b565023ea6a80d
capnslappy@gmail.com <capnslappy@gmail.com@7ae03d0c-6e26-fc45-6741-a244d873c837>
2013-02-16 06:44:06 +0000

    Changes in v1.0.1   * Double-click the tray icon to either launch Pandora One, or bring it to the foreground if it's already active.   * Single application instance support using a mutex

--- PandoraTray/Program.cs
+++ PandoraTray/Program.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace PandoraTray
 {
     static class Program
     {
+        private const string APPLICATION_GUID = "3C2C83C6-7800-11E2-BEF9-B8E36188709B";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            using (System.Threading.Mutex singleInstanceMutex = new System.Threading.Mutex(false, APPLICATION_GUID))
+            {
+                if (singleInstanceMutex.WaitOne(0, false))
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
 
-            ApplicationContext applicationContext = new SystemTrayApplicationContext();
-            Application.Run(applicationContext);
+                    ApplicationContext applicationContext = new SystemTrayApplicationContext();
+                    GC.Collect();
+                    Application.Run(applicationContext);
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
     }
 }
